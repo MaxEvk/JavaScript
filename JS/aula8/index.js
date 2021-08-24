a44d9758f373bd97a85b147681f3eeb297a4f7e6
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-08-24 15:52:05 -0300

    Exercicio

---
+++ JS/aula8/index.js
@@ -0,0 +1,11 @@
+const nome = 'Max';
+const sobreNome = 'Lima';
+const idade = 30;
+const peso = 84;
+const altura = 1.68;
+let imc; //peso / (altura * altura)
+let anoNascimento;
+anoNascimento = '1982';
+imc = peso / (altura * altura);
+
+console.log(nome, sobreNome, 'tem', idade, 'anos, pesa', peso, 'KG, tem', altura, 'de altura e seu IMC é de', imc, nome, sobreNome, 'nasceu em 1982');
