6a139e3abae64950013f5f230ca56418109a27aa
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-08-24 14:53:23 -0300

    Create tests.js

---
+++ aula6/tests.js
@@ -0,0 +1,8 @@
+let nome; //Declarou a variável
+nome = 'João';//inicializando a variável
+console.log(nome);
+nome = 'Otávio';
+console.log(nome);
+//não podemos criar variáveis com palavras reservadas da linguagem
+let n = 'João';//Variáveis precisam ter nomes significativos
+console.log(n);//Não utilize var, utilize LET.
