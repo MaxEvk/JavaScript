cd2bf40e8507c68504109bc4ba9f7b942af6eb03
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-08-24 15:13:41 -0300

    Const

---
+++ JS/aula7/index.js
@@ -0,0 +1,18 @@
+//não se pode modificar o valor de uma constante
+//Não utilize var, utilize const
+//Se quiser modificar o valor, utilize LET
+const nome= 'João';
+console.log(nome);
+
+let nome2;
+nome2 = 'Max';
+console.log(nome2);
+
+//manipulando constantes
+const primeiroNumber = 5;
+const segundoNumber = 10;
+const resultado = primeiroNumber * segundoNumber;
+const resultadoDuplicado = resultado * 2;
+
+console.log(resultado);
+console.log(resultadoDuplicado);
