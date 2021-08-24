55f892ce883c740d621d6c3593399c40270ae403
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-08-24 14:34:10 -0300

    Let

---
+++ aula6/index.js
@@ -0,0 +1,7 @@
+let nome = 'João'; //String
+
+console.log(nome, 'em 1984');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log(nome, 'casou se com Maria em 2012');
+console.log('Maria teve um filho com', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama Eduardo');
