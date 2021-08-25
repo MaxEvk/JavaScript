9aad1d8f2763fc6f7e692f2073fef6d6873661b3
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-08-25 14:41:08 -0300

    Tipos de dados primitivos

---
+++ JS/aula9/index.js
@@ -0,0 +1,8 @@
+const nome = 'Max';//String
+const nome1 = 'Max';//String
+const nome3 = 'Max';//String
+const num1 = 10;//number
+const num2 = 10.52; //number
+let nomeAluno; // undefined
+let sobrenomeAluno = null; // nulo
+const aprovado = false; // boolean = valor true or false.
