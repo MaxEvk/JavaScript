27465d2f49cc7994cfda80ebb854e4400345514f
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-08-30 14:37:05 -0300

    Numbers

---
+++ JS/aula14/index.js
@@ -0,0 +1,13 @@
+let num1 = 0.7
+; //number
+let num2 = 0.1; //number    
+
+//console.log(num1.toString() + num2);
+//num1 = num1.toString();
+//console.log(num1.toString(2)); exibe codigo binário
+//console.log(num1.toFixed(2)); arredondar o número
+//console.log(Number.isInteger(num1));Retorna bolean pra testar se é número inteiro
+/*let temp = num1 * 'Olá';
+console.log(Number.isNaN(temp)); Pergunta se é um NaN (not a number ao sistema)*/
+console.log((num1 + num2).toPrecision(1));
+console.log(Number.isInteger(2));
