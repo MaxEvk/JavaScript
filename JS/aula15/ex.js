b07adbf5b77b5695424431b9c6945c89e74923fc
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-08-31 11:15:21 -0300

    Exercícios aula15

--- JS/aula15/ex.js
+++ JS/aula15/ex.js
@@ -3,9 +3,11 @@ const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
-texto1.innerHTML = `<p>Raiz quadrada: ${numero ** 0.5}</p>`;
-texto2.innerHTML = `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
-texto3.innerHTML = `<p>É NaN: ${Number.isNaN(numero)}</p>`;
-texto4.innerHTML = `<p>Arredondado para baixo: ${Math.floor(numero)}</p>`;
-texto5.innerHTML = `<p>Arredondado para cima: ${Math.ceil(numero)}</p>`;
-texto6.innerHTML = `<p>Com duas casas decimais ${numero.toFixed(2)}</p>`;
+
+texto.innerHTML = ' ';
+texto.innerHTML += `<p>Raiz quadrada: ${numero ** 0.5}</p>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `<p>Arredondado para baixo: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>Arredondado para cima: ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p>Com duas casas decimais ${numero.toFixed(2)}</p>`;
