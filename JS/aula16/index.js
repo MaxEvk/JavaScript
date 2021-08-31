b7f251dad3f03dc60ef6086fc2065f983a03afd5
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-08-31 17:00:27 -0300

    Array

---
+++ JS/aula16/index.js
@@ -0,0 +1,13 @@
+
+const alunos = ['Max', 'Gabriel', 'David'];
+//alunos[3] = 'Eduardo'; add um elemento ao array;
+//alunos.push('Vera'); add no fim;
+//alunos[1] = 'Joel'; alterar o indice;
+//console.log(alunos[0]); acessa o indice;
+//alunos.unshift('Vera'); add no inicio e mudando os elementos
+//const removido1 = alunos.pop(); //remove o elemento do fim do array
+//const removido = alunos.shift();//remove o elemento do inicio
+//delete alunos[1]; deleta o indice 1 do array 
+//console.log(alunos.slice(0, 2)); retorna do indice 0 ao 2
+//console.log(alunos instanceof Array); verifica se é um array
+console.log(alunos);
