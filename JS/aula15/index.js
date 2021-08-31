6ab9d7fe3aefaaa3581440f937b358202f2e3f1d
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-08-31 11:04:08 -0300

    Exercício aula 15

---
+++ JS/aula15/index.js
@@ -0,0 +1,9 @@
+let num1 = 9;
+console.log(num1 ** 0.5);
+//let num2 = Math.floor(num1); método para arredondar um número para baixo;
+//let num2 = Math.ceil(num1);método para arredondar um número para cima;
+//let num2 = Math.round(num1);arredonda para o mais próximo;
+//console.log(Math.max(1,2,3,4,5, -10, -50, -1500, -2511)); retorna o maior número
+//const aleattorio = Math.round( Math.random() * (10 -5) + 5); retorna um número aleatório arredondado entre 10 e -5
+/*console.log(Math.pow(2, 10));
+console.log(2 ** 10); pontência de um número com e sem método */
