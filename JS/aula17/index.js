b6ad0444c5c4f8322637bb2cfff2da2991e4ddc1
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-09-01 13:48:39 -0300

    Funções
    O básico sobre funções

---
+++ JS/aula17/index.js
@@ -0,0 +1,17 @@
+/*function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma(2, 2);
+console.log(resultado);*/
+
+//console.log(soma(2, 2));//passa dois valores para função soma e retorna a const resultado;
+
+const raiz = function (n) {
+    return n ** 0.5;
+}
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
