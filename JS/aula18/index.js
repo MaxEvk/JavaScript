45846b17ec46d06a62c0f7e1c82f0c1e90b8b260
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-09-01 14:38:08 -0300

    Objetos (básico)

---
+++ JS/aula18/index.js
@@ -0,0 +1,52 @@
+const pessoa = {
+    nome: 'Max',
+    sobrenome: 'Lima',
+    idade: 39,
+
+    fala () {
+        console.log(`A minha idade é ${this.idade}`);
+    },
+
+    incrementaIdade(){
+        this.idade++;
+    }
+}
+
+pessoa.fala();
+pessoa.incrementaIdade();
+pessoa.fala();
+pessoa.incrementaIdade();
+pessoa.fala();
+pessoa.incrementaIdade();
+pessoa.fala();
+pessoa.incrementaIdade();
+
+
+
+
+
+
+
+
+
+/*function criaPessoa(nome, sobrenome, idade) {
+    return {
+        nome, sobrenome, idade
+    }
+}
+const pessoa1 = criaPessoa('Max', 'Enon', 39);
+const pessoa2 = criaPessoa('Gabriel', 'Lima', 17);
+const pessoa3 = criaPessoa('David', 'Laurindo', 32);
+const pessoa4 = criaPessoa('João', 'Oliveira', 55);
+const pessoa5 = criaPessoa('Antônio', 'Marques', 47);
+
+console.log(pessoa1.nome, pessoa1.sobrenome, pessoa1.idade);*/
+
+/*const pessoa = {
+    nome: 'Max',
+    sobrenome: 'Lima',
+    idade: 39
+}
+console.log(pessoa.nome);
+console.log(pessoa.sobrenome);
+console.log(pessoa.idade);*/
