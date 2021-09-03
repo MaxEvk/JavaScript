d4f8130f0867e86646e57baa2536b21401f725ea
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-09-03 18:10:03 -0300

    aula21
    Operadores de comparação

---
+++ JS/aula21/index.js
@@ -0,0 +1,12 @@
+/*
+Operadores de comparação
+> maior que
+< menor que
+<= menor ou igual a
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+const comp = 10 > 5;
+console.log (comp);
