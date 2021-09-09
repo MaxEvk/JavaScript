1c043cf63c30492fc7e3754b9f8a09a1f4e66094
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-09-09 15:13:41 -0300

    27Operação ternária

---
+++ JS/aula27/index.js
@@ -0,0 +1,7 @@
+// ? :
+const pontuacaoUsuario = 999;
+//Operação ternária que substitui o if else para testar se uma condição é verdadeira. condição ? faça : senão, faça isso;
+
+const nivelUsuario = pontuacaoUsuario >= 999 ? 'Usuário VIP!' : 'Usuário normal!';
+
+console.log(nivelUsuario);
