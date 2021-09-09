ec4117651870ae14004729b5bbcac7f9a7037aeb
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-09-09 14:44:35 -0300

    Exercício Form

--- JS/aula26/js/main.js
+++ JS/aula26/js/main.js
@@ -1,19 +1,83 @@
 
-    const form = document.querySelector('#form');// select form
-    
-    form.addEventListener('submit', function(evento) {
-        evento.preventDefault();
-        console.log('Evento predefinido');
-        setResultado('Olá mundo');
-    });
+const form = document.querySelector('#form');// select form
 
-    function setResultado(msg) {
-        const resultado = document.querySelector('#resultado');
-        resultado.innerHTML = ' ';
-        const p = document.createElement('p');
-        p.innerHTML = 'Qualquer coisa';
-        resultado.appendChild(p);
+form.addEventListener('submit', function (evento) {
+    evento.preventDefault();
+    const inputPeso = evento.target.querySelector('#peso');
+    const inputAltura = evento.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('peso inválido', false);
+        return;
     }
 
+    if (!altura) {
+        setResultado('Altura inválida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const mensagemUsuário = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(mensagemUsuário, true);
+    console.log(imc, nivelImc);
+    //continua o código...
+});
+
+function getNivelImc(imc) {
+    const nivel = ['Abaixo do peso',
+        'Peso normal',
+        'Sobrepeso',
+        'Obesidade grau 1',
+        'Obesidade grau 2',
+        'Obesidade grau 3'];
+    if (imc >= 39.9) {
+        return nivel[5];
+    } else if (imc >= 34.9) {
+        return nivel[4];
+    } else if (imc >= 29.9) {
+        return nivel[3];
+    } else if (imc >= 24.9) {
+        return nivel[2];
+    } else if (imc >= 18.5) {
+        return nivel[1];
+    } else if (imc < 18.5) {
+        return nivel[0];
+    }
+}
+
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
+
+function criaP(className) {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado(msg, isValid) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = ' ';
+
+    const p = criaP();
+
+    if(isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
     
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+}
+
+
+
+    //console.log(calculo);
 
