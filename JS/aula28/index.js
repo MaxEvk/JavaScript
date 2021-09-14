00f28ed18080223a9a00fa9f738a07dd478da78d
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-09-14 11:17:12 -0300

    Data

--- JS/aula28/index.js
+++ JS/aula28/index.js
@@ -6,7 +6,7 @@ function zeroAEsquerda (n) {
     return n >= 10 ? n : `0${n}`;
 }
 
-function formataData(data1) {
+function formataData(data) {
     const dia = zeroAEsquerda(data.getDate());
     const mes = zeroAEsquerda(data.getMonth() + 1);
     const ano = zeroAEsquerda(data.getFullYear());
