733357b6bf13acc5d2e566972c006d356a9fcd46
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-09-16 16:13:03 -0300

    aula34
    for clássico

---
+++ JS/aula34/index.js
@@ -0,0 +1,13 @@
+//For clássico
+/*
+for (let i = 0; i <= 10; i++) {
+    const par = i % 2 === 0 ? 'par' : 'ímpar';
+    console.log(i, par);
+}*/
+//percorrer um array com laço for
+const frutas = ['Maçã', 'Pera', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    
+    console.log(frutas[i]);
+}
