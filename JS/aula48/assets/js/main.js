0264abbdde67aed631105e6e6f2ab2d0d8a3b2e7
Max Lima <88208329+MaxEvk@users.noreply.github.com>
2021-09-24 16:03:17 -0300

    Aula48
    addEventListener adicionado

--- JS/aula48/assets/js/main.js
+++ JS/aula48/assets/js/main.js
@@ -1,2 +1,8 @@
 const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
  
+btnTarefa.addEventListener('click', function(e){
+    if (!inputTarefa.value) return;
+    console.log(inputTarefa.value);
+});
