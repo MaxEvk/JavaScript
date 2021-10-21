d2f69f16792bc6a24c4594d448ea27442b568aa7
MaxEvk <maxevk@gmail.com>
2021-10-21 17:26:12 -0300

    Rest operator

--- JS/aula50/index.js
+++ JS/aula50/index.js
@@ -1,10 +1,10 @@
-function funcao() {
-    //arguments é uma palavra reservada que contém todos os argumentos da função.Somente quando está em uma function.
+/*function funcao() {
+    //arguments é uma palavra reservada que contém todos os argumentos da função. Somente quando está em uma function.
     console.log(arguments);
 }
-funcao('valor', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+funcao('valor', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);*/
 
-function funcao() {
-    console.log(arguments);
+const conta = (...args) => {
+    console.log(args);
 }
-funcao('valor', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+conta('+', 1, 20, 30, 40, 50);
