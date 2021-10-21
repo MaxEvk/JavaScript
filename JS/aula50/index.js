993e433f7be1efec4b4461d73996efab95fe5363
MaxEvk <maxevk@gmail.com>
2021-10-21 16:35:19 -0300

    arguments

---
+++ JS/aula50/index.js
@@ -0,0 +1,10 @@
+function funcao() {
+    //arguments é uma palavra reservada que contém todos os argumentos da função.Somente quando está em uma function.
+    console.log(arguments);
+}
+funcao('valor', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+function funcao() {
+    console.log(arguments);
+}
+funcao('valor', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
