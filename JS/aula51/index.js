e06836758d2cdd5a30cbfcef94d4b4030b846fa9
MaxEvk <maxevk@gmail.com>
2021-10-22 10:40:43 -0300

    Return

---
+++ JS/aula51/index.js
@@ -0,0 +1,16 @@
+/*document.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'red';
+});*/
+function criaMultiplicador(multiplicador) {
+    return function (n) {
+        return n * multiplicador;
+    };
+};
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadriplica(10));
